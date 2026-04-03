e18bf550daf4da9b0ab832354fcecb5204d7d00b
하루정민 <153725638+jungmin3051@users.noreply.github.com>
2026-04-03 13:25:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from fpdf import FPDF
 # 0. 기본 설정
 st.set_page_config(page_title="법카 영수증 관리", layout="wide")
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1x419Jb6laxcObm4z2nFU_W65Cx-4AxmAjwmE8ouFmjk/edit?usp=sharing"
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection, **st.secrets["connections"]["gsheets"])
 
 def get_meal_priority(meal_name):
     priority = {"조식": 1, "중식": 2, "석식": 3}
